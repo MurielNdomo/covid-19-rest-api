31b81f2e191ba8834455bac237008a50f01d8574
sonia <sonia.ndomo@athena.cm>
2020-04-14 18:32:26 +0100

    Adding logs support

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask
+import datetime
+import time
+import json 
+
+from flask import Flask, request, g
 from flask_restful import Api
 
-from resources.request_estimator import GetEstimator, DefaultGetEstimator
+from resources.request_estimator import GetEstimator, DefaultGetEstimator, GetEstimatorLogs, update_requests_logs
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(DefaultGetEstimator, "/api/v1/on-covid-19")
 api.add_resource(GetEstimator, "/api/v1/on-covid-19/<string:response_format>")
+api.add_resource(GetEstimatorLogs, "/api/v1/on-covid-19/logs")
+
+
+@app.before_request
+def before_request():
+   g.request_start_time = time.time()
+
+@app.after_request
+def after_request_func(response):
+
+    """ 
+    This function will run after a request, as long as no exceptions occur.
+    It must take and return the same parameter - an instance of response_class.
+    This is a good place to do some application cleanup.
+    """
+     # Get elapsed time in milliseconds
+    elapsed = time.time() - g.request_start_time
+    elapsed = int(round(1000 * elapsed))
+    update_requests_logs(dict(method=request.method, path=request.path, status=response.status_code, duration=elapsed))
+    return response
 
 if __name__ == "__main__":
     # app.run(debug=True)
--- resources/request_estimator.py
+++ resources/request_estimator.py
@@ -6,12 +6,36 @@ from resources.estimator import estimator
 import json
 
 
-# outputs data according to the correct format
-def output_xml(data, code, headers=None):
-    """Makes a Flask response with a XML encoded body"""
-    resp = make_response(dumps({'response' :data}), code)
-    resp.headers.extend(headers or {})
-    return resp
+
+def read_requests_logs():
+  """ Opens saved_logs json file and return existing requests logs as list or empty list"""
+  with open("saved_logs.json", "r+") as logs:
+        try:
+            data = json.load(logs)
+        except Exception as e:
+            print('error while loading json data', e)
+            data = []
+        # convert data to list if not
+        if type(data) is dict:
+            data = [data]
+        return data
+
+def update_requests_logs(request_details):
+  """ append new request to requests logs file"""
+  existing_requests = read_requests_logs()
+  with open("saved_logs.json", "r+") as logs:
+    existing_requests.append(request_details)
+    # to reset the file pointer to position 0 
+    logs.seek(0)
+    json.dump(existing_requests, logs)
+
+
+
+
+def getRequestElastedTime(elapsedDelta):
+  # we convert datetime.timedelta(days, seconds, microseconds=) to ms
+  return (elapsedDelta.days * 24 * 60 * 60 + elapsedDelta.seconds) * 1000 + elapsedDelta.microseconds / 1000.0
+
 
 class GetEstimator(Resource):
   def post(self, response_format):
@@ -26,4 +50,13 @@ class GetEstimator(Resource):
 class DefaultGetEstimator(Resource):
   def post(self):
     return estimator(request.get_json(force=True)), 200
-    
+
+def writeResponseLines():
+  return '\n'.join(['{method}  {path}       {status}  {duration}ms'.format(method=elt['method'], path=elt['path'], status=elt['status'], duration=elt['duration']) for elt in read_requests_logs()])
+
+
+class GetEstimatorLogs(Resource):
+  def get(self):
+    response = make_response(writeResponseLines(), 200)
+    response.headers['content-type'] = 'text/plain'
+    return response
